a667497066a76875d116979db5be596ed509089e
kocla <kocla@ruankocoe182>
2016-01-19 18:23:57 +0800

    添加了一些AppStates类

---
+++ 3D-Teris/src/teris/game/states/InputStates.java
@@ -0,0 +1,39 @@
+package teris.game.states;
+
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.renderer.RenderManager;
+
+public class InputStates extends AbstractAppState {
+
+	@Override
+	public void stateAttached(AppStateManager stateManager) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stateDetached(AppStateManager stateManager) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(float tpf) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(RenderManager rm) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void postRender() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 3D-Teris/src/teris/game/states/LogicStates.java
@@ -0,0 +1,7 @@
+package teris.game.states;
+
+import com.jme3.app.state.AbstractAppState;
+
+public class LogicStates extends AbstractAppState {
+
+}
